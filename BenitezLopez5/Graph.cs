5e163a7f4967fd327cda4b49b1d48ee157c18e92
Izak Benitez-Lopez <90430383+KaziKoIzak@users.noreply.github.com>
2023-04-12 14:31:57 -0500

    Interface

--- BenitezLopez5/Graph.cs
+++ BenitezLopez5/Graph.cs
@@ -3,5 +3,34 @@ namespace GraphNS;
 public class Graph: IProcessData, IGraphAlgorithm
 {
     private List<Node>? _nodes;
-    
+
+    private void ResetVistedSet()
+    {
+
+    }
+
+    private Node? FindAdjacentUnvisitedNode(Node node)
+    {
+
+    }
+
+    private static void ViewNode(Node node)
+    {
+
+    }
+
+    public void BreadthFS(int start)
+    {
+
+    }
+
+    public void DepthFS(int start)
+    {
+
+    }
+
+    public void ReadData(string path)
+    {
+
+    }
 }
